bb4556b69dd197133ba0147618ca1d6dce079547
paul <paulnjoroge789016@gmail.com>
2020-09-02 21:11:41 +0300

    di setup

--- app/src/main/java/com/portfolio/advanceddi/room/BlogDatabase.kt
+++ app/src/main/java/com/portfolio/advanceddi/room/BlogDatabase.kt
@@ -1,16 +1,22 @@
 package com.portfolio.advanceddi.room
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import dagger.hilt.android.qualifiers.ApplicationContext
 
 /**
  * Created by paul on 9/2/2020 at 5:40 PM.
  */
-@Database(entities = [BlogCacheEntity::class], version = 1)
-abstract class BlogDatabase {
+@Database(entities = arrayOf(BlogCacheEntity::class), version = 1, exportSchema = false)
+abstract class BlogDatabase : RoomDatabase(){
 
     abstract fun blogDao(): BlogDao
 
     companion object{
         val DATABASE_NAME : String = "blog_db"
+
+
     }
 }
